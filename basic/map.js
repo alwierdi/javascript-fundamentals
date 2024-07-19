c157e19765941ee7394348cafd84b991c5f0f1a8
alwierdi <alwi.erdi4@gmail.com>
2024-07-19 14:05:29 +0700

    .map()

--- basic/map.js
+++ basic/map.js
@@ -1,3 +1,8 @@
+/*
+    -> map berguna untuk membuat array baru yang panjang array nya
+       sama dengan panjang array aslinya, namun elemennya berbeda
+*/
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const squares = numbers.map(square);
 const cubes = numbers.map(cube);
