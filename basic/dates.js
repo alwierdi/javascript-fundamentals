33339d14ac4e5894910b030b6628e3bf91ebe0e1
alwierdi <alwi.erdi4@gmail.com>
2024-08-13 11:22:12 +0700

    feat: datesObject

---
+++ basic/dates.js
@@ -0,0 +1,19 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const dayOfWeek = date.getDay();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
+console.log(dayOfWeek);
+
+
